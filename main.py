2f9b5d91ad5bc21190cda1bd340a7b989172f66f
Rasmus Lindahl <rasmus.lindahl1996@gmail.com>
2023-02-18 10:45:59 +0100

    Add function for plotting in two dimensions

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+import numpy as np
+import matplotlib.pyplot as plt
 from pathlib import Path
 from mininet.net import Mininet
 from mininet.cli import CLI
 from mininet.link import Link, TCLink,Intf
 from subprocess import Popen, PIPE
+import matplotlib.pyplot as plt
 from mininet.log import setLogLevel
 from mininet.clean import Cleanup
 from datetime import datetime
 import time
 import os
 
+# ------ Test run settings ----
+
+diagram_name_comment = "250_Mbps_5_ms_and_5_Mpbs_50_ms_and_100_Mb_transfer"
+#diagram_name_comment = "250_Mbps_5_ms_and_100_Mb_transfer"
+
 config = {}
 
 config["run_count"] = 0
 config["total_number_of_runs"] = 0
 
-config["number_of_paths"] = 2
-config["mptcp_is_enabled"] = False
+config["number_of_paths"] = 1
+config["mptcp_is_enabled"] = True
 
-config["amount_of_bytes_to_request"] = 2 * 1000**3
+config["bytes_to_transfer"] = 0.2 * 1000 ** 3
 
-config["number_of_samples_per_data_point"] = 3
+config["number_of_samples_per_data_point"] = 50
 
 config["client_path_a"] = {}
-config["client_path_a"]["bandwidth"] = 500
-config["client_path_a"]["delay"] = 0
+config["client_path_a"]["bandwidth"] = 250
+config["client_path_a"]["delay"] = 5
 config["client_path_a"]["packet_loss"] = 0
 config["client_path_a"]["queue_size"] = 10
 
 config["client_path_b"] = {}
-config["client_path_b"]["bandwidth"] = 500
-config["client_path_b"]["delay"] = 0
+config["client_path_b"]["bandwidth"] = 3
+config["client_path_b"]["delay"] = 150
 config["client_path_b"]["packet_loss"] = 0
 config["client_path_b"]["queue_size"] = 10
 
@@ -43,11 +53,53 @@ config["server_path"]["queue_size"] = 10
 
 data = []
 
+def run_2d_hist():
+
+    H = np.array([[1, 1, 1, 1],
+                  [1, 1, 1, 1],
+                  [1, 1, 1, 1],
+                  [1, 1, 1, 1.2]])  
+
+    plt.imshow(H)
+
+    cdict = {'red':  ((0.0, 0.0, 0.0),   
+                      (0.5, 1.0, 1.0),    
+                      (1.0, 0.8, 0.8)),   
+
+            'green': ((0.0, 0.8, 0.8),    
+                      (0.5, 1.0, 1.0),   
+                      (1.0, 0.0, 0.0)),  
+
+            'blue':  ((0.0, 0.0, 0.0),   
+                      (0.5, 1.0, 1.0),    
+                      (1.0, 0.0, 0.0))   
+           }
+
+    GnRd = colors.LinearSegmentedColormap('GnRd', cdict)
+    fig,ax = plt.subplots(1)
+    p=ax.pcolormesh(H,cmap=GnRd,vmin=-0.5,vmax=1.5)
+    fig.colorbar(p,ax=ax)
+
+    now = datetime.now()
+    date_time = now.strftime("_%m_%d_%Y__%H_%M_%S")
+    diagram_path = diagram_name_comment + '_experiment' + date_time + '.png'
+
+    plt.savefig(diagram_path)
+    os.system("feh " + diagram_path+ " &")
+
 def main():
 
     print()
     print("---- Starting test bed -----")
     print()
+
+    diagrams_folder = "diagrams"
+
+    # Create folder for diagrams
+    try:
+        os.mkdir(diagrams_folder)
+    except:
+        pass
 
     path = ""
     parameter = ""
@@ -103,40 +155,42 @@ def main():
     plt.boxplot(data)
 
     if path != "" and parameter != "":
-        plt.xlabel(parameter + " for " + path)
+        plt.xlabel(parameter + " for " + path + " (sample size: " + str(config["number_of_samples_per_data_point"]) + ")")
+    else:
+        plt.xlabel("(sample size: " + str(config["number_of_samples_per_data_point"]) + ")")
 
     plt.ylabel("Transfer time (seconds)")
 
     now = datetime.now()
     date_time = now.strftime("_%m_%d_%Y__%H_%M_%S")
 
-    plt.savefig('experiment' + date_time + '.png')
-    plt.show()
+    diagram_path = diagrams_folder + "/" + diagram_name_comment + '_experiment' + date_time + '.png'
+    plt.savefig(diagram_path)
+    os.system("feh " + diagram_path+ " &")
+
+def delete_file(file_path):
+
+    if os.path.exists(file_path):
+        os.remove(file_path)
 
 def run_cmd(net, cmd_str):
 
-    script_name = "temp.sh"
-    output_name = "temp.out"
+    script_name = ".temp.sh"
+    output_name = ".temp.out"
     
-    if os.path.exists(script_name):
-        os.remove(script_name)
+    delete_file(script_name)
+    delete_file(output_name)
 
-    if os.path.exists(output_name):
-        os.remove(output_name)
-
-    f = open(script_name, 'a+')  # open file in append mode
+    f = open(script_name, 'a+')
     f.write(cmd_str + " > " + output_name)
     f.close()
 
     CLI(net, script=script_name)
 
-    if os.path.exists(script_name):
-        os.remove(script_name)
-
     res = Path(output_name).read_text()
 
-    if os.path.exists(output_name):
-        os.remove(output_name)
+    delete_file(script_name)
+    delete_file(output_name)
 
     return res
 
@@ -153,11 +207,19 @@ def runConfig():
         print()
 
         run_cmd(net, "h2 python3 server.py &")
+
         time.sleep(1)
-        res = run_cmd(net, "h1 python3 client.py 2000000000")
-        print(res)
+
+        res = run_cmd(net, "h1 python3 client.py " + str(int(config["bytes_to_transfer"])))
+        data_value = float(res.split("Total time: ")[-1].split(" ")[0])
+
+        print("Total time:", data_value, "seconds")
+
+        print()
 
         net.stop()
+
+        data_point.append(data_value)
 
         i += 1
         config["run_count"] += 1
@@ -222,13 +284,13 @@ def initMininet():
     h2.cmd("ip route add default via 10.0.2.1 dev h2-eth0 table 1")
     h2.cmd("ip route add default scope global nexthop via 10.0.2.1 dev h2-eth0")
 
-    '''
     if config["number_of_paths"] == 1:
         h1.cmd("ip link set h1-eth1 down")
-    '''
 
     return net, h1, h2
 
 if '__main__' == __name__:
-    main()
+    run_2d_hist()
+    #main()
+
 
